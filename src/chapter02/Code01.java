bd1c88d08943345ee22f071a329b86d3dc9a7610
JeongYoungseo <wjddudtjjys@gmail.com>
2025-03-17 11:55:56 +0900

    main Mathod 사용법, 연결연산자, 덧셈연산자,printf,println,print

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Code01 {
+    public static void main (String[] args){
+        System.out.println("aisoftware");
+        System.out.println("jeongyoungseo");
+        System.out.printf("grade1\n");
+        System.out.println("result"+"100+200");
+        System.out.println(100+200+"won");
+    }
+}
